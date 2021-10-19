41fbe4fcec8fe6306932047db2797e75abc5f689
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-19 18:57:32 +0100

    Clean up main function

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,7 @@
 #include "amqp/AmqpConnection.hpp"
 #include "amqp/AmqpChannel.hpp"
 
-nlohmann::json getRabbitMqData ();
-
 int main () {
-    nlohmann::json rmqData = getRabbitMqData();
-    
-    AmqpConnection connection(rmqData);
-
-    while (true) {}
 
     return 0;
-}
-
-nlohmann::json getRabbitMqData () {
-    nlohmann::json rabbitMqData ;
-
-    
-
-    return rabbitMqData;
 }
