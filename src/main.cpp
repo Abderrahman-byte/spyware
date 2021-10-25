90cd21175b588c0538b7949c78e87b7c33d84a47
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-25 20:37:56 +0100

    Authenticate

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,16 @@
 
 #include "config.h"
 #include "core/utils.hpp"
-
+#include "core/authentication.hpp"
 #include "amqp/AmqpClient.hpp"
 
 int main () {
     AmqpClient amqpClient (getRmqConfig());
+    std::string token;
+
     amqpClient.open();
 
+    token = authenticate(amqpClient, AUTH_USER, AUTH_PASSWORD);
 
     while (true) {}
 
