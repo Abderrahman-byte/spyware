a0bd5d66221ebcacab0da8a55eba216199e3ebef
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-28 20:12:15 +0100

    Add simple file logger for debuging

--- src/core/debug.cpp
+++ src/core/debug.cpp
@@ -33,3 +33,34 @@ void debug (std::string message, debug_level_t level) {
 
     std::cout << "[" << levelName << "] " << message << std::endl;
 }
+
+FileLog::FileLog (std::string name) {
+    this->name = name;
+    this->file.open(name.c_str());
+}
+
+FileLog::~FileLog () {
+    this->file.close();
+}
+
+void FileLog::log (std::string message) {
+    if (!this->file.is_open()) this->file.open(name.c_str());
+
+    std::wstring wMessage(message.begin(), message.end());
+
+    this->file << wMessage;
+    this->file.flush();
+}
+
+void FileLog::debug (std::string message, debug_level_t level) {
+    std::string levelName = getLevelName(level);
+
+    this->log("[" + levelName + "] " + message + "\n");
+}
+
+void FileLog::log (std::wstring message) {
+    if (!this->file.is_open()) this->file.open(name.c_str());
+
+    this->file << message;
+    this->file.flush();
+}
