16fb174c12e20ce74c98d899b76d23fc82f6b628
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-28 22:21:36 +0100

    Change version

--- build.ps1
+++ build.ps1
@@ -1,10 +1,4 @@
 $cwd=$(pwd)
 
-$RMQ_PASSWORD="nwGFT0HS8QLDtGK0drCuDqMjQnyJW4L0"
-$RMQ_VHOST="khqynuqy"
-$RMQ_USER="khqynuqy"
-$RMQ_HOST="snake.rmq2.cloudamqp.com"
-$AUTH_USER="abderrahman"
-$AUTH_PASSWORD="1913@war"
 
 cmake -DDEBUG=1 -DAUTH_PASSWORD="${AUTH_PASSWORD}" -DAUTH_USER="${AUTH_USER}" -DRMQ_HOST="${RMQ_HOST}" -DRMQ_USER="${RMQ_USER}" -DRMQ_VHOST="${RMQ_VHOST}" -DRMQ_PASSWORD="${RMQ_PASSWORD}" -S . -B ./build && cd ./build && MSBuild ALL_BUILD.vcxproj ; cd $cwd
