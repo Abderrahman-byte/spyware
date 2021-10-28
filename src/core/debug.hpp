895ed07c6abdfc2681d36a6afa0f14cc68f4f027
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-10-28 20:12:37 +0100

    Add simple file logger for debuging

--- src/core/debug.hpp
+++ src/core/debug.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <fstream>
 
 #ifndef _SPYWARE_DEBUG_HPP_
 #define _SPYWARE_DEBUG_HPP_ 1
@@ -12,5 +13,18 @@ typedef enum debug_level {
 } debug_level_t ;
 
 extern void debug (std::string, debug_level_t = LEVEL_DEBUG);
+
+class FileLog {
+    private :
+        std::string name;
+        std::wofstream file;
+
+    public :
+        FileLog (std::string);
+        ~FileLog ();
+        void log(std::string);
+        void log (std::wstring);
+        void debug (std::string, debug_level_t = LEVEL_DEBUG);
+};
 
 #endif
